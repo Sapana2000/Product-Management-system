bc7ce7c8dd88e3fda7a1a6cfc1a232cf5fd2214a
Sapana Sonawane <sapanasonawane5@gmail.com>
2025-09-24 16:02:30 +0530

    Initial commit

---
+++ application/views/web/pages/pdf.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Dompdf Practise</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+	<img src="http://imSapana Sonawane.com/wp-content/uploads/2017/06/Sapana Sonawane-Cv.jpg" alt="" />
+	<p>Copyright</p>
+</body>
+</html>
